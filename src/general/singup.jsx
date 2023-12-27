f30448b4d55a7f54250dedb8b2951833b0cff20f
SaraCohen1 <sara149635@gmail.com>
2023-12-27 14:40:09 +0200

    14:40

--- src/general/singup.jsx
+++ src/general/singup.jsx
@@ -1,14 +1,12 @@
-import React, { useContext } from 'react'
-import Cookies from 'js-cookie';
+import React from 'react';
 import { useForm } from 'react-hook-form';
+import { Button, TextField, Typography, Container, CssBaseline, Avatar, Paper } from '@mui/material';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import { API_URL, apiRequestMethod } from '../services/apiService';
 import { useNavigate } from 'react-router-dom';
-import { AppContext } from '../context/context';
 
 const SignUp = () => {
-    const { register, handleSubmit, formState: { errors }, getValues } = useForm();
-    // const { user, setUser } = useContext(AppContext);
-
+    const { register, handleSubmit, formState: { errors } } = useForm();
     const navigate = useNavigate();
 
     const onSubmitSignUp = async (data) => {
@@ -31,38 +29,54 @@ const SignUp = () => {
         }
     };
 
-    return (<div className="">
-        <div>SignUp</div>
-        <form onSubmit={handleSubmit(onSubmitSignUp)} className="mt-3">
-            <div className="mb-4">
-                <label className="block text-sm font-medium text-gray-700">
-                    Name:
-                </label>
-                <input {...register('name', { required: true, minLength: 2 })} type="text" className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500" />
-                {errors.name && <div className="text-red-500 text-xs">name is required and must be at least 2 characters long</div>}
-            </div>
-            <div className="mb-4">
-                <label className="block text-sm font-medium text-gray-700">
-                    Email:
-                </label>
-                <input {...register('email', { required: true, pattern: /^\S+@\S+$/i })} type="email" className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500" />
-                {errors.email && <div className="text-red-500 text-xs">Email is required and must be a valid email address</div>}
-            </div>
-            <div className="mb-4">
-                <label htmlFor="password" className="block text-sm font-medium text-gray-700">
-                    Password:
-                </label>
-                <input {...register('password', { required: true, minLength: 6 })} type="password" className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500" />
-                {errors.password && errors.password.type === 'required' && <div className="text-red-500 text-xs">Password is required</div>}
-                {errors.password && errors.password.type === 'minLength' && <div className="text-red-500 text-xs">Password must be at least 6 characters long</div>}
-            </div>
-            <button type="submit" className="bg-purple-500 text-white px-4 py-2 rounded-md mt-4">
-                SignUp
-            </button>
+    return (
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <Paper elevation={3} style={{ padding: '2rem', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Sign Up
+                </Typography>
+                <form onSubmit={handleSubmit(onSubmitSignUp)} style={{ width: '100%', marginTop: '1rem' }}>
+                    <TextField
+                        {...register('name', { required: true, minLength: 2 })}
+                        margin="normal"
+                        fullWidth
+                        label="Name"
+                        error={Boolean(errors.name)}
+                        helperText={errors.name && "Name is required and must be at least 2 characters long"}
+                    />
+                    <TextField
+                        {...register('email', { required: true, pattern: /^\S+@\S+$/i })}
+                        margin="normal"
+                        fullWidth
+                        label="Email"
+                        error={Boolean(errors.email)}
+                        helperText={errors.email && "Email is required and must be a valid email address"}
+                    />
+                    <TextField
+                        {...register('password', { required: true, minLength: 6 })}
+                        margin="normal"
+                        fullWidth
+                        label="Password"
+                        type="password"
+                        error={Boolean(errors.password)}
+                        helperText={errors.password && (errors.password.type === 'required' ? "Password is required" : "Password must be at least 6 characters long")}
+                    />
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        style={{ marginTop: '1rem', background: '#4CAF50', color: 'white' }}
+                    >
+                        Sign Up
+                    </Button>
+                </form>
+            </Paper>
+        </Container>
+    );
+};
 
-        </form>
-    </div>
-    )
-}
-
-export default SignUp
+export default SignUp;
