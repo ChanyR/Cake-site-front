68e0a6bb1a985e36869d8c4aafc19fbddf2d6934
ChanyR <c7195671@gmail.com>
2023-12-27 11:40:17 +0200

    11:40
    Co-authored-by: RikiGerlitz <RikiGerlitz@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { AppContext } from './context/context'
+import Login from './general/login'
+import { BrowserRouter } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [user, setUser] = useState([]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <AppContext.Provider value={{ user, setUser }}>
+        <Login />
+      </AppContext.Provider>
+    </BrowserRouter>
+
   )
 }
 
--- src/services/apiService.jsx
+++ src/services/apiService.jsx
@@ -1,13 +1,13 @@
 import axios from 'axios'
+import Cookies from 'js-cookie';
+export const API_URL = "http://localhost:3001";
+// export const TOKEN_NAME = "ADMIN_TOKEN";
 
-export const API_URL = "http://localhost:3001"
-export const TOKEN_NAME = "ADMIN_TOKEN"
-
-export const doApiGet = async (_url) => {
+export const apiRequestGet = async (_url) => {
     try {
         let resp = await axios.get(_url, {
             headers: {
-                "x-api-key": localStorage[TOKEN_NAME]
+                "x-api-key": Cookies.get('token')
             }
         })
         return resp;
@@ -18,15 +18,17 @@ export const doApiGet = async (_url) => {
 }
 
 // For Post,delete, put, patch
-export const doApiMethod = async (_url, _method, _body = {}) => {
+export const apiRequestMethod = async (_url, _method, _body = {}) => {
     try {
         console.log(_body)
+        console.log(_method);
+        console.log(_url);
         let resp = await axios({
             url: _url,
             method: _method,
             data: _body,
             headers: {
-                "x-api-key": localStorage[TOKEN_NAME]
+                "x-api-key": Cookies.get('token')
             }
         })
         return resp;
