5342ffac08d6532f315d759b1e7917eb1334b7eb
ChanyR <c7195671@gmail.com>
2023-12-27 15:40:30 +0200

    15:40

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
 
         <Routes>
           {/* <Route path="/admin/*" element={<HeaderAdmin />} />
-        <Route path="/test/*" element={<HeaderTest />} /> */}
+          <Route path="/test/*" element={<HeaderTest />} /> */}
           <Route path="/*" element={<ClientNav />} />
         </Routes>
 
