388925e4005b7627049a1dcec9c66c52f9876903
RikiGerlitz <rikig4114@gmail.com>
2023-12-28 14:22:11 +0200

    riki14:22

--- src/bakers/baker.jsx
+++ src/bakers/baker.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Card, CardContent, Typography, CardActions, Button, Grid } from '@mui/material';
-// import FavoriteIcon from '@mui/icons-material/Favorite';
 import ThumbUpOffAltOutlinedIcon from '@mui/icons-material/ThumbUpOffAltOutlined';
+import { API_URL, apiRequestMethod } from '../services/apiService';
+import { AppContext } from '../context/context';
 
 
 const Baker = (props) => {
+ 
   let item = props.item;
+  let handleLikeClick=props.handleLikeClick;
+
+
+  
 
   return (
     <Grid item xs={12} md={6}>
@@ -25,7 +31,15 @@ const Baker = (props) => {
           </Typography>
         </CardContent>
         <CardActions>
-        <ThumbUpOffAltOutlinedIcon sx={{ marginRight: '8px' }} />
+          
+       
+          <Typography variant="body2" color="text.secondary">
+            {item.likes}
+          </Typography>
+          <Button size="small" color="primary" >
+          <ThumbUpOffAltOutlinedIcon sx={{ marginRight: '8px' }} onClick={()=>{handleLikeClick(item._id)}} />
+          {/* {console.log(item.likes+1)}; */}
+          </Button>
           <Button size="small" color="primary">
             View Details
           </Button>
