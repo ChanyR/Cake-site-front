68cd8ca42fdc5f4907a94e65be185c44a2167509
ChanyR <c7195671@gmail.com>
2024-01-04 10:05:12 +0200

    chany 10:05

--- src/users/userOrders.jsx
+++ src/users/userOrders.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
-import { PayPalButton } from "react-paypal-button-v2"
-import Paypal from '../general/paypal';
 
 const UserOrders = () => {
   return (
     <div>
       <div>UserOrders</div>
-      <Paypal/>
     </div>
   )
 }
